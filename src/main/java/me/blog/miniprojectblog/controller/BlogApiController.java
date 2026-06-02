5508f0494509f23eabad10e78cdb944d3c651313
wlsdbwls <wlsdbwls614@gmail.com>
2026-06-03 01:25:08 +0900

    feat[게시글 등록]

---
+++ src/main/java/me/blog/miniprojectblog/controller/BlogApiController.java
@@ -0,0 +1,27 @@
+package me.blog.miniprojectblog.controller;
+
+import lombok.RequiredArgsConstructor;
+import me.blog.miniprojectblog.domain.Article;
+import me.blog.miniprojectblog.dto.AddArticleRequest;
+import me.blog.miniprojectblog.service.BlogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class BlogApiController {
+
+    private final BlogService blogService;
+
+    @PostMapping("/api/articles")
+    public ResponseEntity<Article> addArticle(@RequestBody AddArticleRequest request) {
+        Article savedArticle = blogService.save(request);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(savedArticle);
+    }
+
+}
