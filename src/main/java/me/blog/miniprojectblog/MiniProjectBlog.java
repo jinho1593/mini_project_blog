6fb933492fadf37a9d2737d4e683fc1b1c328629
wlsdbwls <wlsdbwls614@gmail.com>
2026-06-02 20:51:50 +0900

    feat[build.gradle] 스프링부트 초기설정

---
+++ src/main/java/me/blog/miniprojectblog/MiniProjectBlog.java
@@ -0,0 +1,11 @@
+package me.blog.miniprojectblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniProjectBlog {
+    public static void main(String[] args) {
+        SpringApplication.run(MiniProjectBlog.class, args);
+    }
+}
